4b434df54bfb6e7c23e136d61cb1740a668f4e2a
miquel toni <mmayol13167@alumnes.iesmanacor.cat>
2021-02-25 20:11:56 +0100

    Primera practica amb Json_2

--- WS_1.php
+++ WS_1.php
@@ -31,4 +31,15 @@ echo print_r($jmperezperez)."<br>";
 $jmperez_json = json_encode($jmperezperez);
 echo var_dump($jmperez_json)."<br>";
 echo print_r($jmperez_json)."<br>";
+
+$elements_3='{"jmperezperez":true, "thelinmichael":false, "wizzler": false}';
+echo "3 elements string format" . "/n";
+echo var_dump($elements_3)."<br>";
+echo print_r($elements_3)."<br>";
+
+$result_arr_ass = json_decode($elements_3,true);
+echo "jmperezperez".$result_arr_ass["jmperezperez"]."<br>";
+echo "thelinmichael".$result_arr_ass["thelinmichael"]."<br>";
+echo "wizzler".$result_arr_ass["wizzler"]."<br>";
+    
 ?>
